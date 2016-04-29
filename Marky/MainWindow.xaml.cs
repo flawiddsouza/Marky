bf8f07f84f8ed5cb42847760597d84e118bf225a
Flawid DSouza <flawid@live.in>
2016-04-29 07:07:14 +0530

    Updated CefSharp + Added Refresh + Added Markdown Themes

--- Marky/MainWindow.xaml.cs
+++ Marky/MainWindow.xaml.cs
@@ -14,16 +14,16 @@ namespace Marky
         public static RoutedUICommand Preferences = new RoutedUICommand("Preferences", "Preferences", typeof(MainWindow));
         public static RoutedUICommand ExportHTML = new RoutedUICommand("Export to HTML", "ExportHTML", typeof(MainWindow));
         public string MarkdownFilePath { get; set; }
-        private string CSS;
-        private string renderedMarkdown;
-        private string html;
-        private bool pageLoaded; // check if any item has been loaded into the browser component
+        private string _css;
+        private string _renderedMarkdown;
+        private string _html;
+        private bool _pageLoaded; // check if any item has been loaded into the browser component
 
         public MainWindow()
         {
             InitializeComponent();
 
-            CSS = new MarkdownFlavorSelector().GetCSS();
+            _css = new MarkdownFlavorSelector().GetCSS();
 
             Properties.Settings.Default.SettingChanging += Default_SettingChanging;
         }
@@ -42,18 +42,18 @@ namespace Marky
             {
                 var m = new MarkdownDeep.Markdown(); // alternative: MarkdownSharp.Markdown(); rest of the syntax is the same
                 string markdown = await Helpers.ReadAllLinesAsync(MarkdownFilePath);
-                renderedMarkdown = m.Transform(markdown);
-                html = "<html>\n<head>\n<style>\n"+ CSS + "</style>\n</head>\n<body oncontextmenu='return true'>\n" + renderedMarkdown + "</body>\n</html>";
-                Viewer.LoadHtml(html, "http://renderpage/");
-                pageLoaded = true;
+                _renderedMarkdown = m.Transform(markdown);
+                _html = "<html>\n<head>\n<style>\n"+ _css + "</style>\n</head>\n<body oncontextmenu='return true'>\n" + _renderedMarkdown + "</body>\n</html>";
+                Viewer.LoadHtml(_html, "http://renderpage/");
+                _pageLoaded = true;
                 ExportToHTML.IsEnabled = true;
             }
         }
 
         private void RefreshView()
         {
-            html = "<html>\n<head>\n<style>\n" + CSS + "</style>\n</head>\n<body oncontextmenu='return true'>\n" + renderedMarkdown + "</body>\n</html>";
-            Viewer.LoadHtml(html, "http://renderpage/");
+            _html = "<html>\n<head>\n<style>\n" + _css + "</style>\n</head>\n<body oncontextmenu='return true'>\n" + _renderedMarkdown + "</body>\n</html>";
+            Viewer.LoadHtml(_html, "http://renderpage/");
         }
 
         private void Copy_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
@@ -76,24 +76,39 @@ namespace Marky
             e.CanExecute = true;
         }
 
+        private void Refresh_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            RefreshView();
+        }
+
+        private void Refresh_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (_pageLoaded)
+            {
+                e.CanExecute = true;
+            }
+        }
+
         private void ExportHTML_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            var dlg = new SaveFileDialog();
-            dlg.FileName = Path.GetFileNameWithoutExtension(MarkdownFilePath);
-            dlg.DefaultExt = ".html";
-            dlg.Filter = "HTML document (.html)|*.html";
+            var dlg = new SaveFileDialog
+            {
+                FileName = Path.GetFileNameWithoutExtension(MarkdownFilePath),
+                DefaultExt = ".html",
+                Filter = "HTML document (.html)|*.html"
+            };
 
             var result = dlg.ShowDialog();
 
             if (result == true)
             {
-                File.WriteAllText(dlg.FileName, html);
+                File.WriteAllText(dlg.FileName, _html);
             }
         }
 
         private void ExportHTML_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            if(pageLoaded)
+            if(_pageLoaded)
             {
                 e.CanExecute = true;
             }
@@ -132,7 +147,7 @@ namespace Marky
 
         private void Default_SettingChanging(object sender, System.Configuration.SettingChangingEventArgs e)
         {
-            CSS = new MarkdownFlavorSelector().GetCSS();
+            _css = new MarkdownFlavorSelector().GetCSS();
             RefreshView();
         }
     }
